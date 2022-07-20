670768ab6ff2787c1cbff2749434542145bb61f3
Flavia <fermoselleflavia@gmail.com>
2022-07-20 20:11:13 -0300

    terminado

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -246,7 +246,11 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return "Hola" + nombre + "!";
+  
+  
+    return "Hola" + " " + nombre + "!";
+    
+  
   
   
   
