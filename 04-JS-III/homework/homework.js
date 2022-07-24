66890ab99c9c5bfb193020770a8b592165860429
Flavia <fermoselleflavia@gmail.com>
2022-07-24 17:45:53 -0300

    fatlan 2

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -137,6 +137,15 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+     var mayor=numeros[0]
+
+    for(var i=0; i < numeros.length; i++) {
+       if (numeros[i] > mayor){
+        mayor = numeros[i];
+       } 
+
+ }
+       return mayor;
 
  }  
   
@@ -145,11 +154,35 @@ function numeroMasGrande(numeros) {
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí: 
 
+   if (arguments.length === 0) return 0;
+   if ( arguments.length === 1){
+      return arguments[0];
+   } 
+    
+     var aux=arguments[0]
 
-}
+   for(var i= 1; i< arguments.length; i++){
+  
+      aux= (aux * arguments[i]) 
+    
+  } 
+     return aux;
+  }
+
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
+  
+  
+  var cantidad= 0
+
+  for(var i=0; i< arreglo.length; i++){
+      if(arreglo[i] > 18){
+        cantidad= cantidad +1;
+      }
+
+  } 
+ return cantidad ;
 
 }
 
@@ -159,6 +192,16 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
+ if (numeroDeDia === 1 || numeroDeDia === 7 ) {
+   return "Es fin de semana";
+  
+ } else {
+   return "Es dia Laboral";
+  
+ }
+
+
+
   
 } 
 
@@ -167,6 +210,14 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
+    str= n + " ";
+  if (str[0] === "9") {
+    return true; 
+    
+  } else {
+    return false;
+    
+  }
   
 }
 
@@ -174,16 +225,44 @@ function empiezaConNueve(n) {
 function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
-  //Escribe tu código aquí  
-  
-} 
+  //Escribe tu código aquí 
+
+
+    for( var i= 1; i < arreglo.length; i++){
+      if (arreglo[i] !== arreglo[0]) {
+        return false;
+        }
+    
+    }    
+     
+      return true;
+  } 
+
 
 
 function mesesDelAño(array) {
   //Dado un array que contiene algunos meses del año desordenados, recorrer el array buscando los meses de 
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
-  //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
+  //Si alguno de los meses no está, devolver:"No se encontraron los meses pedidos"
   // Tu código:
+
+  var mesespedidos=[]
+ 
+  for(var i=0; i< array.length; i++){
+     var nuevoarray= array[i]
+    if (nuevoarray === "Marzo" || nuevoarray === "Noviembre"|| nuevoarray === "Enero"){
+             mesespedidos.push(nuevoarray); }
+            
+  
+ }
+    if (mesespedidos.length !== 3) {
+      return "No se encontraron los meses pedidos";
+      
+    }
+    return mesespedidos;
+
+
+
 }
 
 
@@ -191,6 +270,19 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+
+  nuevoarray=[]
+
+  for(var i=0; i< array.length; i++){
+      if (array[i] > 100) {
+      
+     nuevoarray.push(array[i]);
+  }
+
+}
+   return nuevoarray;
+
+
 }
 
 
