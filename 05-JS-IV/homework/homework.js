ea965bb95a547d5f26d3057540db5ab854302ce6
Flavia <fermoselleflavia@gmail.com>
2022-07-25 12:11:51 -0300

    Completo

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -124,6 +124,11 @@ function pasarUsuarioAPremium (usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
+   
+   for(var i=0; i< usuarios.length; i++){
+       usuarios[i].esPremium= true;
+
+   }return usuarios;
 }
 
 function sumarLikesDeUsuario (usuario) {
@@ -133,6 +138,16 @@ function sumarLikesDeUsuario (usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+
+  var suma = 0;
+
+  for ( var i = 0; i < usuario.posts.length; i++){
+    suma = usuario.posts[i].likes + suma;
+  }
+         return suma;
+
+
+
 }
 
 function agregarMetodoCalculoDescuento (producto) {
@@ -145,7 +160,13 @@ function agregarMetodoCalculoDescuento (producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
+ 
 
+    producto.calcularPrecioDescuento = function () {return this.precio - this.precio * this.porcentajeDeDescuento};
+     
+ 
+      return producto;
+  
 }
 
 // No modificar nada debajo de esta línea
