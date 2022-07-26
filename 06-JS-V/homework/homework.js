3e7581ef269493701ff22130381f955ecf8a3f89
Flavia <fermoselleflavia@gmail.com>
2022-07-26 14:02:45 -0300

    1

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -1,5 +1,7 @@
 // No cambies los nombres de las funciones.
 
+const { stringify } = require("git-url-parse");
+
 function crearUsuario() {
   // Crea una Clase de ES6 o una función constructor llamada "Usuario"
   // Debe aceptar un objeto "opciones" con las propiedades "usuario", "nombre", "email" y "password"
@@ -8,12 +10,33 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+  class Usuario { constructor(opciones){
+    this.usuario = opciones.usuario,
+    this.nombre = opciones.nombre,
+    this.email = opciones.email,
+    this.password = opciones.password,
+    this.saludar = function () {
+      return "Hola, mi nombre es "+ this.nombre;
+      
+    }
+  }
+      
+  }return Usuario;
 }
+
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+
+
+   Constructor.prototype.saludar = function() { 
+    return "Hello World!";
+    
+  } 
+
+
 }
 
 function agregarStringInvertida() {
@@ -22,7 +45,19 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
-}
+
+  var string = {nombre:Toni, }
+ 
+  var stringinvertida = []
+
+ string.prototype.reverse() = stringinvertida;
+
+
+  
+ }
+
+
+
 
 // ---------------------------------------------------------------------------//
   //Crea el constructor de la clase "Persona"
@@ -37,10 +72,14 @@ function agregarStringInvertida() {
 
   class Persona {
     constructor(/*Escribir los argumentos que recibe el constructor*/) {
-      // Crea el constructor:
-
+     // Crea el constructor:
+       
     }
-}
+    }  
+
+
+    
+
 
 function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
